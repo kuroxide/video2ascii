daf0709ac217b5a241eb885440a736abc448b5bf
Neon4272 <neon4272main@gmail.com>
2021-04-05 01:34:15 +1000

    Add argument variables for integration
    Clean up formatting

--- txt2png.bat
+++ txt2png.bat
@@ -1,14 +1,19 @@
-@echo off
+@ECHO OFF
 
-set /A count = 1
+CD ./output
 
-:x
+SET WIDTH=%1
+SET HEIGHT=%2
+SET /A FRAMES=%3
+SET /A count = 1
 
-convert -size WIDTHxHEIGHT xc:white -font "C:\WINDOWS\FONTS\LUCON.TTF" -pointsize 12 -fill black -annotate +0+0 "@output%count%.txt" render\frame%count%.png
-echo Rendered frame %count%
+:X
 
-set /A count = %count% + 1
+CONVERT -size %WIDTH%x%HEIGHT% xc:white -font "C:\WINDOWS\FONTS\LUCON.TTF" -pointsize 12 -fill black -annotate +0+9 "@output%count%.txt" render\frame%count%.png
+ECHO Rendered frame %count% to PNG
 
-if %count% LEQ frames goto x
+SET /A count = %count% + 1
 
-timeout 60
+IF %count% LEQ %FRAMES% goto X
+
+TIMEOUT 15
